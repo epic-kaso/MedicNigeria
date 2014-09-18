8de25b4fe96726225383f8b105b94c4b42fa6417
Akachukwu Okafor <kasoprecede47@gmail.com>
2014-09-18 14:54:03 +0100

    initial

---
+++ app/views/pages/landing_page.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Grayscale - Start Bootstrap Theme</title>
+
+    <?= stylesheet_link_tag() ?>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                    <i class="fa fa-stethoscope"></i> <span class="light">Medic</span> Nigeria
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#faq">Faq</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">Medic Nigeria</h1>
+                        <p class="intro-text">Find. Book Appointments.</p>
+                        <p class="intro-text">With Medical Specialist Nearest to You.</p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <span class="fa fa-bullhorn fa-3x text-center"></span>
+                <h1>We are currently in private better</h1>
+                <p>You can request an invitation to test our service.</p>
+                <!-- email text field -->
+                {{ Form::open() }}
+                <div class="form-group">
+                   {{ Form::text('email',null,['class'=>'form-control','placeholder'=>'Email goes here']) }}
+                </div>
+                <div class="form-group">
+                    {{ Form::submit('Send Me an Invitation!',['class'=>'btn btn-default btn-lg']) }}
+                </div>
+                {{ Form::close() }}
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <hr/>
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; landar-studio 2014</p>
+        </div>
+    </footer>
+    <?= javascript_include_tag() ?>
+</body>
+
+</html>
